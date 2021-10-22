3be3f0901fe10e0b0237a34b6095e8f2731fda15
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-22 22:05:18 +0100

    adding tests and function for def test_room_starts_empty

--- classes/room.py
+++ classes/room.py
@@ -5,7 +5,8 @@ class Room:
         self.guest_list = []
         self.songs = []
 
-        
+    def room_guest_list_count(self):
+        return len(self.guest_list)
 
     def check_in_guest(self,guest_to_check_in):
         self.guest_list.append(guest_to_check_in)
--- tests/room_test.py
+++ tests/room_test.py
@@ -13,9 +13,9 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(5,self.room.entry_fee)
 
     def test_room_starts_empty(self):
-        self.assertEqual(0,len(self.room.guest_list)
+        self.assertEqual(0, self.room.room_guest_list_count())
 
-    # def test_check_in_guest(self):
-    #     guest_to_check_in = Guest("Alan",40,"Last Christmas")
-    #     self.room.check_in_guest(guest_to_check_in)
-    #     self.assertEqual(1,len(self.guest_list))
+    def test_check_in_guest(self):
+        guest_to_check_in = Guest("Alan",40,"Last Christmas")
+        self.room.check_in_guest(guest_to_check_in)
+        self.assertEqual(1,self.room.room_guest_list_count())
